49052a669a01d7cb0bcf18b7ee4d61d65544d1c8
Khairil Yusof <khairil.yusof@gmail.com>
2021-05-05 13:10:23 +0800

    Initial fields for Mining Concession

--- src/odm/naturalresources/content/mining_concession.py
+++ src/odm/naturalresources/content/mining_concession.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from collective import dexteritytextindexer
+from z3c.relationfield.schema import RelationChoice
+from zope import schema
 from zope.interface import implementer
+from plone.app.vocabularies.catalog import CatalogSource
 
-
-# from odm.naturalresources import _
+from odm.naturalresources import _
 
 
 class IMiningConcession(model.Schema):
@@ -21,40 +24,31 @@ class IMiningConcession(model.Schema):
 
     # model.load('mining_concession.xml')
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    start_date = schema.Date(
+        title=_(u'Start Date'),
+        description=_(u'Date which this concession began'),
+        required=False,)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    end_date = schema.Date(
+        title=_('End Date'),
+        description=_(u'Date which this concession ended'),
+        required=False,)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    # Licensed To
+    directives.widget('licensed_to',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                          'basePath': '/',
+                          'mode': 'auto',
+                          'favourites': [],
+                      },
+                      )
+    licensed_to = RelationChoice(
+        title=_(u'Licensed To'),
+        description=_(u'The organization licensed was issued to.'),
+        source=CatalogSource(portal_type='Organization'),
+        required=False,
+    )
 
 
 @implementer(IMiningConcession)
